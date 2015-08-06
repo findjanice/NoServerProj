a93a52b83a1a9d84c4e401d401492d8cc17718a8
Janice <findjanice@gmail.com>
2015-08-06 09:57:55 -0600

    changed sidebar nav

--- js/directive/dirColor.js
+++ js/directive/dirColor.js
@@ -2,7 +2,7 @@ app.directive('dirColor', function() {
   return {
     restrict: "E",
     transclude: true,
-    template: '  '
+    template: '<div class = "col-md-12" style="border: 1px dashed #C4C4C4; border-radius: 10px; margin: 10px; padding: 10px"><ng-transclude></ng-transclude> </div>
   }
   //end of directive
 })
